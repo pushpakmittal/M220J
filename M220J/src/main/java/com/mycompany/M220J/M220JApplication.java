64023d286e1cc5548092cf80d4e579390489a478
HP <HP@LAPTOP-KBOFEJ6E>
2021-06-07 22:56:16 +0530

    initial commit

---
+++ M220J/src/main/java/com/mycompany/M220J/M220JApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.M220J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M220JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M220JApplication.class, args);
+	}
+
+}
---
+++ M220J/src/test/java/com/mycompany/M220J/M220JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.M220J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M220JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
